8638fa2cd74b9b21d225d28183947d2963fcb5b9
Farolan Faisal <farolanfaisal@gmail.com>
2019-12-02 07:49:05 +0800

    first

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,27 @@
+// Imports
+import plugin0 from '/home/farolan/prj/juwal/juwal-frontend/node_modules/react-static-plugin-reach-router/browser.api.js'
+
+// Plugins
+const plugins = [{
+        location: "/home/farolan/prj/juwal/juwal-frontend/node_modules/react-static-plugin-source-filesystem",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/home/farolan/prj/juwal/juwal-frontend/node_modules/react-static-plugin-reach-router",
+        plugins: [],
+        hooks: plugin0({})
+      },
+{
+        location: "/home/farolan/prj/juwal/juwal-frontend/node_modules/react-static-plugin-sitemap/dist",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/home/farolan/prj/juwal/juwal-frontend",
+        plugins: [],
+        hooks: {}
+      }]
+
+// Export em!
+export default plugins
---
+++ artifacts/react-static-templates.js
@@ -0,0 +1,14 @@
+
+
+  
+// Template Map
+export default {
+  '/home/farolan/prj/juwal/juwal-frontend/src/pages/404.js': require('/home/farolan/prj/juwal/juwal-frontend/src/pages/404.js').default,
+'/home/farolan/prj/juwal/juwal-frontend/src/pages/about.js': require('/home/farolan/prj/juwal/juwal-frontend/src/pages/about.js').default,
+'/home/farolan/prj/juwal/juwal-frontend/src/pages/blog.js': require('/home/farolan/prj/juwal/juwal-frontend/src/pages/blog.js').default,
+'/home/farolan/prj/juwal/juwal-frontend/src/pages/index.js': require('/home/farolan/prj/juwal/juwal-frontend/src/pages/index.js').default,
+'/home/farolan/prj/juwal/juwal-frontend/src/containers/Post': require('/home/farolan/prj/juwal/juwal-frontend/src/containers/Post').default
+}
+
+export const notFoundTemplate = '/home/farolan/prj/juwal/juwal-frontend/src/pages/404.js'
+
