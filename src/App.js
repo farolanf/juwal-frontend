467fdd67e9956b763c01d2c840b66ed318f549df
Farolan Faisal <farolanfaisal@gmail.com>
2019-12-02 09:22:35 +0800

    update layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Root, Routes, addPrefetchExcludes } from 'react-static'
-//
+
+import { Container, Segment } from 'semantic-ui-react'
 import { Link, Router } from 'components/Router'
 import Dynamic from 'containers/Dynamic'
-
-import './app.css'
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
 addPrefetchExcludes(['dynamic'])
@@ -18,14 +17,21 @@ function App() {
         <Link to="/blog">Blog</Link>
         <Link to="/dynamic">Dynamic</Link>
       </nav>
-      <div className="content">
-        <React.Suspense fallback={<em>Loading...</em>}>
-          <Router>
-            <Dynamic path="dynamic" />
-            <Routes path="*" />
-          </Router>
-        </React.Suspense>
-      </div>
+      <Container>
+        <main>
+          <React.Suspense fallback={<em>Loading...</em>}>
+            <Router>
+              <Dynamic path="dynamic" />
+              <Routes path="*" />
+            </Router>
+          </React.Suspense>
+        </main>
+        <footer>
+          <Segment vertical>
+            Copyright {new Date().getFullYear()} Juwal
+          </Segment>
+        </footer>
+      </Container>
     </Root>
   )
 }
