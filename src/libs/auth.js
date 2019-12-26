9d1e5a818265e5284a3218b3d7ae0e198639797d
Farolan Faisal <farolanfaisal@gmail.com>
2019-12-27 07:46:24 +0800

    add localStorage shim

--- src/libs/auth.js
+++ src/libs/auth.js
@@ -1,3 +1,9 @@
+const localStorage = global.localStorage ? global.localStorage : {
+  setItem() {},
+  getItem() {},
+  removeItem() {}
+}
+
 export const setToken = token => localStorage.setItem('token', token)
 
 export const getToken = () => localStorage.getItem('token')
