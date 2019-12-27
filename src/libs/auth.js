d2009fcd13678e4d2c7b2b244f2ac558f99317c6
Farolan Faisal <farolanfaisal@gmail.com>
2019-12-27 09:20:15 +0800

    handle no token

--- src/libs/auth.js
+++ src/libs/auth.js
@@ -8,6 +8,6 @@ export const setToken = token => localStorage.setItem('token', token)
 
 export const getToken = () => localStorage.getItem('token')
 
-export const getBearerToken = () => `Bearer ${getToken()}`
+export const getBearerToken = () => getToken() ? `Bearer ${getToken()}` : ''
 
 export const removeToken = () => localStorage.removeItem('token')
